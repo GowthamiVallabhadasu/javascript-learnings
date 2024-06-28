86875d59fe6bac8ce87546d137e116f85cded2a9
Gowthami Vallabhadasu <46236089+GowthamiVallabhadasu@users.noreply.github.com>
2024-06-28 11:11:17 +0000

    init

---
+++ semicolons.js
@@ -0,0 +1,9 @@
+// both will give the same output
+
+console.log("hello world")
+console.log("testing"); 
+
+// this will give error , hence commenting
+//a=3+4 console.log(a)
+
+a=3+4;console.log(a) //works but bad practice as the code is not readable. 
