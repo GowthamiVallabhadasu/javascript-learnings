70bc414bdc74d72a6a69a2ea5bfb4a5458fd9971
Gowthami Vallabhadasu <46236089+GowthamiVallabhadasu@users.noreply.github.com>
2024-06-28 10:14:02 +0000

    init

---
+++ variables.js
@@ -0,0 +1,11 @@
+const name="Gowthami"
+var email="gowthamivallabhadasu@gmail.com"
+let password="12345test!"
+lastName="Vallabhadasu"
+
+console.log(name);
+console.log(email);
+console.log(password);
+console.log(lastName);
+
+console.table([name,email,password])
