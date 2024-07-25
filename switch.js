19f71b11372d83e59e2bce66a01b36f644b5f80b
Gowthami Vallabhadasu <46236089+GowthamiVallabhadasu@users.noreply.github.com>
2024-07-25 19:22:50 +0000

    init

---
+++ switch.js
@@ -0,0 +1,64 @@
+// let a=2+2
+// switch (a){
+
+//     case 2:
+//         {
+//         console.log(a)
+//         break
+//         }
+//     case 4:
+//         {
+//             console.log(a)
+//             break
+//         }
+//     default:
+//         {
+//         console.log("Nothing!")
+//         break
+//         }
+// }
+
+// grouing of switch
+// let age =18
+// switch (age){
+//     case 17:
+//         {
+//             console.log("Under age")
+//             break
+//         }
+//     case (18):
+//     case (19):
+//         {
+//             console.log("Welcome!")
+//             break
+//         }
+//     default:{
+//         console.log("No age specified")
+//         break
+//     }
+// }
+
+// expressions as switch case arguments
+// let a=4
+// switch(a+1){
+//     case 4-1:
+//         {
+//             console.log("Okay")
+//             break
+//         }
+//     case 4+1:
+//         {
+//             console.log("Some value")
+//         }
+//     default:
+//         console.log("Default")
+// }
+
+// type is in the switch - case
+let a="2"
+switch(a){
+    case 2:
+        console.log("Hi")
+    case "2":
+        console.log("This will execute")
+}
